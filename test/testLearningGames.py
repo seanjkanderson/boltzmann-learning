df9c366a31410298951f083cf11792e46132a2a5
Sean Anderson <seananderson@ucsb.edu>
2024-08-08 16:31:49 -0700

    added equivalent tests for continuous measurements

--- test/testLearningGames.py
+++ test/testLearningGames.py
@@ -1,9 +1,6 @@
 import unittest
-import numpy
+import numpy as np
 
-import sys
-
-sys.path.append("src")
 import LearningGames
 
 
@@ -12,66 +9,66 @@ class Test010RandomIntegers(unittest.TestCase):
 
     def test010_get_random_integer1(self):
         print("\ntest_get_random_integer1:")
-        rng = numpy.random.default_rng(5)
+        rng = np.random.default_rng(5)
         p = [3]
 
         M = 10000
 
         ks = [LearningGames.get_random_integer(rng, p) for i in range(M)]
-        histogram = numpy.zeros(len(p))
+        histogram = np.zeros(len(p))
         for k in ks:
             histogram[k] += 1
         histogram /= M
-        err = histogram - numpy.array(p) / sum(p)
+        err = histogram - np.array(p) / sum(p)
         print("  actual distribution:    ", p)
         print("  number of samples:      ", M)
         print("  empirical distribution: ", histogram)
         print("  error:                  ", err)
-        print("  rms error:              ", numpy.linalg.norm(err) / M)
+        print("  rms error:              ", np.linalg.norm(err) / M)
 
-        self.assertLess(numpy.linalg.norm(err) / M, 1e-5)
+        self.assertLess(np.linalg.norm(err) / M, 1e-5)
 
     def test011_get_random_integer4(self):
         print("\ntest_get_random_integer4:")
-        rng = numpy.random.default_rng(5)
+        rng = np.random.default_rng(5)
         p = [0.2, 0.1, 0.5, 0.2]
 
         M = 10000
 
         ks = [LearningGames.get_random_integer(rng, p) for i in range(M)]
-        histogram = numpy.zeros(len(p))
+        histogram = np.zeros(len(p))
         for k in ks:
             histogram[k] += 1
         histogram /= M
-        err = histogram - numpy.array(p) / sum(p)
+        err = histogram - np.array(p) / sum(p)
         print("  actual distribution:    ", p)
         print("  number of samples:      ", M)
         print("  empirical distribution: ", histogram)
         print("  error:                  ", err)
-        print("  rms error:              ", numpy.linalg.norm(err) / M)
+        print("  rms error:              ", np.linalg.norm(err) / M)
 
-        self.assertLess(numpy.linalg.norm(err) / M, 1e-5)
+        self.assertLess(np.linalg.norm(err) / M, 1e-5)
 
     def test012_get_random_integer5(self):
         print("\ntest_get_random_integer5:")
-        rng = numpy.random.default_rng(5)
+        rng = np.random.default_rng(5)
         p = [0.2, 0.1, 0.5, 0.2, 0.0]
 
         M = 10000
 
         ks = [LearningGames.get_random_integer(rng, p) for i in range(M)]
-        histogram = numpy.zeros(len(p))
+        histogram = np.zeros(len(p))
         for k in ks:
             histogram[k] += 1
         histogram /= M
-        err = histogram - numpy.array(p) / sum(p)
+        err = histogram - np.array(p) / sum(p)
         print("  actual distribution:    ", p)
         print("  number of samples:      ", M)
         print("  empirical distribution: ", histogram)
         print("  error:                  ", err)
-        print("  rms error:              ", numpy.linalg.norm(err) / M)
+        print("  rms error:              ", np.linalg.norm(err) / M)
 
-        self.assertLess(numpy.linalg.norm(err) / M, 1e-5)
+        self.assertLess(np.linalg.norm(err) / M, 1e-5)
 
 
 class Test020LearningGames(unittest.TestCase):
@@ -85,10 +82,10 @@ class Test020LearningGames(unittest.TestCase):
         print(vars(lg))
 
         # Check object variables
-        self.assertEqual(lg.action_set, action_set)
-        self.assertEqual(lg.measurement_set, measurement_set)
-        self.assertEqual(lg.min_cost, +numpy.inf)
-        self.assertEqual(lg.max_cost, -numpy.inf)
+        self.assertEqual(lg._action_set, action_set)
+        self.assertEqual(lg._measurement_set, measurement_set)
+        self.assertEqual(lg.min_cost, +np.inf)
+        self.assertEqual(lg.max_cost, -np.inf)
         self.assertEqual(lg.inverse_temperature, 0.01)
         self.assertEqual(len(lg.energy), len(measurement_set))
         for m in measurement_set:
@@ -110,10 +107,10 @@ class Test020LearningGames(unittest.TestCase):
         print("  empirical distribution: ", histogram)
         err = [v / M - 1 / len(action_set) for (k, v) in histogram.items()]
         print("  error:                  ", err)
-        print("  rms error:              ", numpy.linalg.norm(err) / M)
+        print("  rms error:              ", np.linalg.norm(err) / M)
 
         self.assertEqual(len(err), len(action_set))
-        self.assertLess(numpy.linalg.norm(err) / M, 1e-5)
+        self.assertLess(np.linalg.norm(err) / M, 1e-5)
 
     def test022_update_energies(self):
         print("\ntest_update_energies:")
@@ -179,6 +176,7 @@ class Test020LearningGames(unittest.TestCase):
         self.assertLess(regret, regret_bound)
 
     def test024_regret_gamma(self):
+        # TODO: rename this test as gamma is no longer a parameter?
         print("\ntest023_regret_gamma:")
         action_set = {"a1", "a2", "a3"}
         measurement_set = {"y1", "y2"}
@@ -193,6 +191,138 @@ class Test020LearningGames(unittest.TestCase):
         for i in range(M):
             lg.update_energies("y1", reward1)
             lg.update_energies("y2", reward2)
+        # print(vars(lg))
+
+        (
+            average_cost,
+            minimum_cost,
+            cost_bound,
+            regret,
+            regret_bound,
+            steps,
+            alpha1,
+            alpha0,
+        ) = lg.get_regret(display=True)
+
+        self.assertEqual(lg.min_cost, 1.0)
+        self.assertEqual(lg.max_cost, 3.0)
+        self.assertLess(average_cost, cost_bound)
+        self.assertLess(regret, regret_bound)
+
+
+class TestContinuousMeasurementLearningGames(unittest.TestCase):
+    """TestLearningGames Class to test LearningGames constructor"""
+    # TODO: likely can combine this with test for finite measurement class with proper use of setUp/tearDown
+    def test_get_action(self):
+        print("\ntest_get_action:")
+        action_set = {"a1", "a2", "a3"}
+        measurement_set = {"y1", "y2"}
+        lg = LearningGames.LearningGame(action_set, measurement_set, seed=0,
+                                        finite_measurements=False)
+
+        # Check distribution from get_action (starts as uniform)
+        M = 10000
+        measurement = {"y1": 1., "y2": 0.}
+        As = [lg.get_action(measurement)[0] for _ in range(M)]
+        histogram = {k: 0 for k in action_set}
+        for a in As:
+            histogram[a] += 1
+        print("  number of samples:      ", M)
+        print("  empirical distribution: ", histogram)
+        err = [v / M - 1 / len(action_set) for (k, v) in histogram.items()]
+        print("  error:                  ", err)
+        print("  rms error:              ", np.linalg.norm(err) / M)
+
+        self.assertEqual(len(err), len(action_set))
+        self.assertLess(np.linalg.norm(err) / M, 1e-5)
+
+    def test_update_energies(self):
+        print("\ntest_update_energies:")
+        action_set = {"a1", "a2", "a3"}
+        measurement_set = {"y1", "y2"}
+        # use low temperature to converge to deterministic very fast
+        lg = LearningGames.LearningGame(
+            action_set, measurement_set, inverse_temperature=10.0, seed=0,
+            finite_measurements=False
+        )
+
+        # with low temperature, all probability goes to lowest energy
+        reward = {"a1": 1.0, "a2": 2.0, "a3": 3.0}
+        measurement = {"y1": 1., "y2": 0.}
+        lg.update_energies(measurement, reward)
+        print(vars(lg))
+
+        # regret: before update all actions have equal probability
+        self.assertAlmostEqual(lg.total_cost, 2.0)
+
+        # new distribution: check distribution from get_action (starts as uniform)
+        M = 10000
+        As = [lg.get_action(measurement)[0] for _ in range(M)]
+        histogram = {k: 0 for k in action_set}
+        for a in As:
+            histogram[a] += 1
+        print("  number of samples:      ", M)
+        print("  empirical distribution: ", histogram)
+
+        self.assertGreater(histogram["a1"], M - 2)
+        self.assertLess(histogram["a2"], 2)
+        self.assertLess(histogram["a3"], 2)
+
+    def test_regret_T(self):
+        print("\ntest023_regret_T:")
+        action_set = {"a1", "a2", "a3"}
+        measurement_set = {"y1", "y2"}
+        # use low temperature to converge to deterministic very fast, but bound is bad
+        lg = LearningGames.LearningGame(
+            action_set, measurement_set, inverse_temperature=0.01, seed=0,
+            finite_measurements=False
+        )
+
+        M = 10000
+        reward1 = {"a1": 1.0, "a2": 2.0, "a3": 3.0}
+        reward2 = {"a1": 1.0, "a2": 2.0, "a3": 3.0}
+        for _ in range(M):
+            measurement_y1 = {"y1": 1., "y2": 0.}
+            lg.update_energies(measurement_y1, reward1)
+            measurement_y2 = {"y1": 0., "y2": 1.}
+            lg.update_energies(measurement_y2, reward2)
+        # print(vars(lg))
+
+        (
+            average_cost,
+            minimum_cost,
+            cost_bound,
+            regret,
+            regret_bound,
+            steps,
+            alpha1,
+            alpha0,
+        ) = lg.get_regret(display=True)
+
+        self.assertEqual(lg.min_cost, 1.0)
+        self.assertEqual(lg.max_cost, 3.0)
+        self.assertLess(average_cost, cost_bound)
+        self.assertLess(regret, regret_bound)
+
+    def test_regret_gamma(self):
+        # TODO: rename this test as gamma is no longer a parameter?
+        print("\ntest023_regret_gamma:")
+        action_set = {"a1", "a2", "a3"}
+        measurement_set = {"y1", "y2"}
+        # use low temperature to converge to deterministic very fast, but bound is bad
+        lg = LearningGames.LearningGame(
+            action_set, measurement_set, inverse_temperature=0.01, seed=0,
+            finite_measurements=False
+        )
+
+        M = 20000
+        reward1 = {"a1": 1.0, "a2": 2.0, "a3": 3.0}
+        reward2 = {"a1": 1.0, "a2": 2.0, "a3": 3.0}
+        for _ in range(M):
+            measurement_y1 = {"y1": 1., "y2": 0.}
+            lg.update_energies(measurement_y1, reward1)
+            measurement_y2 = {"y1": 0., "y2": 1.}
+            lg.update_energies(measurement_y2, reward2)
         # print(vars(lg))
 
         (
